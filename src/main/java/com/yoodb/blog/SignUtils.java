63db9d27fde27c80632be67aa28d286e29e61bbc
afreon <afreon@163.com>
2017-05-08 10:10:17 +0800

    Create SignUtils.java

---
+++ src/main/java/com/yoodb/blog/SignUtils.java
@@ -0,0 +1,44 @@
+package com.yoodb.blog;
+
+public class SignUtils {
+	public String originalUrl = "org.wso2.carbon.service.mgt.ui_4.7.0";
+	
+	public String filePath = "E:\\org.wso2.carbon.service.mgt.ui_4.7.0";
+	
+	public String jarPath = "E:\\org.wso2.carbon.service.mgt.ui_4.7.0.jar";
+	
+	public String jarNewPath = "E:\\org.wso2.carbon.service.mgt.ui_4.7.1.jar";
+
+	public String getOriginalUrl() {
+		return originalUrl;
+	}
+
+	public void setOriginalUrl(String originalUrl) {
+		this.originalUrl = originalUrl;
+	}
+
+	public String getFilePath() {
+		return filePath;
+	}
+
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+
+	public String getJarPath() {
+		return jarPath;
+	}
+
+	public void setJarPath(String jarPath) {
+		this.jarPath = jarPath;
+	}
+
+	public String getJarNewPath() {
+		return jarNewPath;
+	}
+
+	public void setJarNewPath(String jarNewPath) {
+		this.jarNewPath = jarNewPath;
+	}
+	
+}
