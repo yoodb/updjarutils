d6a5c5de7038c78c16fff1e78a2c057b9bee7117
yoodb <afreon@163.com>
2017-05-08 11:00:00 +0800

    Update SignUtils.java

--- src/main/java/com/yoodb/blog/SignUtils.java
+++ src/main/java/com/yoodb/blog/SignUtils.java
@@ -1,12 +1,24 @@
 package com.yoodb.blog;
 
 public class SignUtils {
+	/**
+	 * 用于压缩包删除前缀目录
+	 */
 	public String originalUrl = "org.wso2.carbon.service.mgt.ui_4.7.0";
 	
+	/**
+	 * 解压后的文件夹目录
+	 */
 	public String filePath = "E:\\org.wso2.carbon.service.mgt.ui_4.7.0";
 	
+	/**
+	 * 需要解压的文件夹
+	 */
 	public String jarPath = "E:\\org.wso2.carbon.service.mgt.ui_4.7.0.jar";
 	
+	/**
+	 * 新打包后的压缩包名称
+	 */
 	public String jarNewPath = "E:\\org.wso2.carbon.service.mgt.ui_4.7.1.jar";
 
 	public String getOriginalUrl() {
