c678d67080900af6ac0e827bec5ec7dde1284488
yoodb <afreon@163.com>
2017-05-08 14:08:18 +0800

    Update Tests.java

--- src/test/java/com/yoodb/blog/Tests.java
+++ src/test/java/com/yoodb/blog/Tests.java
@@ -1,7 +1,13 @@
 package com.yoodb.blog;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Properties;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -17,6 +23,8 @@ public class Tests {
 	 * @throws Exception
 	 */
 	public static void main(String[] args) throws Exception {
+		String logInfoPath = "E://info.log";
+		FileOutputStream logfile = new FileOutputStream(logInfoPath);
 		String proName ="Resources";
 		ArrayList<File> filelist = UpdJarContent.searchJarFiles("E://");
 		for (File file : filelist) {
@@ -24,6 +32,8 @@ public class Tests {
 			SignUtils ss = new SignUtils();
 			String path = file.getAbsolutePath();
 			ss.setJarPath(path);
+			logfile.write(("------" + path + "------").getBytes());
+			logfile.write("\r\n".getBytes());
 			log.info("需要解压的压缩包路径-->" + path);
 			String fileName = file.getName().replace(".jar", "");
 			ss.setOriginalUrl(fileName);
@@ -34,7 +44,11 @@ public class Tests {
 			String newFilePath = filePath.substring(0, filePath.length()-1) + "_1.jar";
 			ss.setJarNewPath(newFilePath);
 			log.info("新打包后的压缩包路径-->" + newFilePath);
-			ut.updJarContent(ss,proName);
+			ut.updJarContent(ss,logfile,proName);
+			logfile.write(("------" + path + "------").getBytes());
+			logfile.write("\r\n".getBytes());
 		}
+		logfile.close();
 	}
+	
 }
