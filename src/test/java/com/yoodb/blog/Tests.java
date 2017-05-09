7d2607fd7d7e86ec27b844f2aba6c1093bdcab49
yoodb <afreon@163.com>
2017-05-09 13:45:04 +0800

    Update Tests.java

--- src/test/java/com/yoodb/blog/Tests.java
+++ src/test/java/com/yoodb/blog/Tests.java
@@ -17,11 +17,18 @@ public class Tests {
 	 * @param args
 	 * @throws Exception
 	 */
+	/**
+	 * 
+	 * @param args
+	 * @throws Exception
+	 */
 	public static void main(String[] args) throws Exception {
-		String logInfoPath = "E://info.log";
-		FileOutputStream logfile = new FileOutputStream(logInfoPath);
+		String infoPath = "E:\\files\\";
+		String logFilePath = "info.log";
+		UpdJarContent.createFile(infoPath, logFilePath);
+		FileOutputStream logfile = new FileOutputStream(infoPath + logFilePath);
 		String proName ="Resources";
-		ArrayList<File> filelist = UpdJarContent.searchJarFiles("E://");
+		ArrayList<File> filelist = UpdJarContent.searchJarFiles("E:\\wso2esb-5.0.1-SNAPSHOT\\repository\\components\\plugins");
 		for (File file : filelist) {
 			UpdJarContent ut = new UpdJarContent();
 			SignUtils ss = new SignUtils();
@@ -30,20 +37,24 @@ public class Tests {
 			logfile.write(("------" + path + "------").getBytes());
 			logfile.write("\r\n".getBytes());
 			log.info("需要解压的压缩包路径-->" + path);
+			
 			String fileName = file.getName().replace(".jar", "");
 			ss.setOriginalUrl(fileName);
 			log.info("用于压缩包删除前缀目录的名称-->" + fileName);
-			String filePath = path.replace(".jar", "");
+			
+			String filePath = infoPath + fileName;
 			ss.setFilePath(filePath);
 			log.info("解压后的文件夹目录路径-->" + filePath);
-			String newFilePath = filePath.substring(0, filePath.length()-1) + "_1.jar";
+			
+			String newFilePath = infoPath + file.getName();
 			ss.setJarNewPath(newFilePath);
 			log.info("新打包后的压缩包路径-->" + newFilePath);
+			
 			ut.updJarContent(ss,logfile,proName);
 			logfile.write(("------" + path + "------").getBytes());
 			logfile.write("\r\n".getBytes());
-			DeleteDirectory.deleteDir(new File(filePath));
-			log.info("删除临时解压文件 -->" + filePath + "<-- 成功！");
+			//DeleteDirectory.deleteDir(new File(filePath));
+			//log.info("删除临时解压文件 -->" + filePath + "<-- 成功！");
 		}
 		logfile.close();
 	}
